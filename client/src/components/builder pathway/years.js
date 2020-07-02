253e02bd4d7df0a5edd1ddde89cf7cb4090a9c6c
emmanuel iyanu <emmanueliyanu2012@gmail.com>
2020-07-02 01:21:27 +0100

    selecting yearly

--- client/src/components/builder pathway/years.js
+++ client/src/components/builder pathway/years.js
@@ -35,14 +35,34 @@ export class Years extends Component {
                                     <p>We'll find the best templates for your experience level.</p>
                                 </div>
                                 <form onSubmit={this.handleFormSubmit}>
-                                    <ul className="ul-number ul-num1">
-                                        <Link><li name="year" value="option1" checked={this.state.selectedOption === "option1"} onChange={this.handleOptionChange} className="yr-x1">0</li></Link>
-                                        <Link><li name="year" value="option2" checked={this.state.selectedOption === "option2"} onChange={this.handleOptionChange} style={{ backgroundColor: this.state.bgColor, color: this.state.color }} className="yr-x2">3</li></Link>
-                                        <Link><li name="year" value="option3" checked={this.state.selectedOption === "option3"} onChange={this.handleOptionChange} className="yr-x3">5</li></Link>
-                                        <Link><li name="year" value="option4" checked={this.state.selectedOption === "option4"} onChange={this.handleOptionChange} className="yr-x4">10</li></Link>
-                                        <Link><li name="year" value="option5" checked={this.state.selectedOption === "option5"} onChange={this.handleOptionChange} className="yr-x5">10+</li></Link>
-                                    </ul>
 
+                                    <ul className="ul-number">
+                                        <li className="form-check">
+                                            <label>
+                                                <input type="radio" name="react-tips" value="option1" checked={this.state.selectedOption === "option1"} onChange={this.handleOptionChange} className="form-check-input"
+                                                /> 1  </label>
+                                        </li>
+                                        <li className="form-check">
+                                            <label>
+                                                <input type="radio" name="react-tips" value="option2" checked={this.state.selectedOption === "option2"} className="form-check-input" onChange={this.handleOptionChange}
+                                                /> 3  </label>
+                                        </li>
+                                        <li className="form-check">
+                                            <label>
+                                                <input type="radio" name="react-tips" value="option3" checked={this.state.selectedOption === "option3"} className="form-check-input" onChange={this.handleOptionChange}
+                                                /> 5 </label>
+                                        </li>
+                                        <li className="form-check">
+                                            <label>
+                                                <input type="radio" name="react-tips" value="option4" checked={this.state.selectedOption === "option4"} className="form-check-input" onChange={this.handleOptionChange}
+                                                /> 7 </label>
+                                        </li>
+                                        <li className="form-check">
+                                            <label>
+                                                <input type="radio" name="react-tips" value="option5" checked={this.state.selectedOption === "option5"} className="form-check-input" onChange={this.handleOptionChange}
+                                                /> 10 </label>
+                                        </li>
+                                    </ul>
                                     <div className="create pt-5">
                                         <Link to="/launch" className="btn-create mr-4"><i className="fa fa-backward"></i></Link>
                                         <Link type="submit" to="/template" className="btn-create"><i className="fa fa-forward"></i></Link>
