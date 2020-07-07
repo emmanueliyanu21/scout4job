5afa73b86bf3894e6aa7c4f647458de8cbbcc0d8
emmanuel iyanu <emmanueliyanu2012@gmail.com>
2020-07-07 16:11:23 +0100

    adding online database structure

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/cv-builder",
+    // mongoURI: "mongodb://localhost:27017/cv-builder",
+    mongoURI: "mongodb://scoutjob:12345Abcd@ds031965.mlab.com:31965/scout4job",
     secretOrKey: "secret"
 };
 
