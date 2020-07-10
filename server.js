bea72b490b9d400ede3c23ada1d0bd1eb7d694a6
emmanuel iyanu <emmanueliyanu2012@gmail.com>
2020-07-10 10:20:55 +0100

    few changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const http = require('http');
 
 const users = require("./routes/api/users");
 
@@ -40,3 +41,22 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
 
+process
+    .on('SIGTERM', shutdown('SIGTERM'))
+    .on('SIGINT', shutdown('SIGINT'))
+    .on('uncaughtException', shutdown('uncaughtException'));
+
+setInterval(console.log.bind(console, 'tick'), 1000);
+http.createServer((req, res) => res.end('hi'))
+    .listen(process.env.PORT || 3000, () => console.log('Listening'));
+
+function shutdown(signal) {
+    return (err) => {
+        console.log(`${signal}...`);
+        if (err) console.error(err.stack || err);
+        setTimeout(() => {
+            console.log('...waited 5s, exiting.');
+            process.exit(err ? 1 : 0);
+        }, 5000).unref();
+    };
+}
