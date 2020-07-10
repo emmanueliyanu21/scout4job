e0250decb91fed42aa6e7d4c931a156bca457e0b
emmanuel iyanu <emmanueliyanu2012@gmail.com>
2020-07-10 09:20:36 +0100

    few changes

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
-const posts = require("./routes/api/posts");
 
 const app = express();
 
@@ -36,7 +35,6 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
-app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
